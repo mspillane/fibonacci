2518992e9eec1813b8f97afd4854acdcd8994514
mspillane <mspillane@cartrawler.com>
2018-03-29 00:34:16 +0100

    Improve unit tests

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,22 +1,22 @@
-import json
 import pytest
 import unittest
-from unittest import mock
 from fibonacci.core.fibonacci_calculator import get_fibonacci_numbers
-from fibonacci.api.fibonacci_api import app
 
 class TestFibonacciGenerator(unittest.TestCase):
     """
-    Tests the Fibonacci generator and REST API
+    Tests the Fibonacci generator
     """
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
     def test_get_fibonacci_sequence(self):
         fib_numbers = get_fibonacci_numbers(10)
         assert isinstance(fib_numbers, list)
         assert fib_numbers == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+    def test_get_fibonacci_corner_cases(self):
+        fib_numbers = get_fibonacci_numbers(0)
+        assert isinstance(fib_numbers, list)
+        assert fib_numbers == []
+
 
     def test_get_fibonacci_sequence_fails_for_non_integer_argument(self):
         expected_msg = 'The quantity parameter must be a valid integer. Invalid parameter=abc'
@@ -32,33 +32,3 @@ class TestFibonacciGenerator(unittest.TestCase):
             get_fibonacci_numbers(-1)
         assert str(ex.value) == expected_msg
 
-'''
-class TestFibonacciApi(unittest.TestCase):
-    """
-    """
-    def setUp(self):
-        self.app = app.test_client()
-
-
-    def test_fibonacci_success(self):
-        response = self.app.get('/fibonacci/5')
-        assert response.status_code == 200
-        fibonacci_dict = json.loads(response.get_data().decode())
-        sequence = fibonacci_dict['fibonacci']
-        assert sequence == [0, 1, 1, 2, 3]
-
-
-    # def test_thing(self):
-    #     response = self.app.get('/')
-    #     assert response == expected
-
-
-
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-'''
