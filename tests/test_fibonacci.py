233bcfba0e9a36f5a92347861bef01e331d8d6c9
mspillane <mspillane@cartrawler.com>
2018-03-29 17:12:13 +0100

    Use pytest parameterization to test multiple sets of data

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,21 +1,37 @@
 import pytest
 import unittest
+
 from fibonacci.core.fibonacci_calculator import get_fibonacci_numbers
+
+
+@pytest.mark.parametrize(
+    'number_of_items, expected', [
+        (0, []),
+        (1, [0]),
+        (2, [0, 1]),
+        (3, [0, 1, 1]),
+        (5, [0, 1, 1, 2, 3]),
+    ]
+)
+def test_get_fibonacci_corner_cases(number_of_items, expected):
+    fib_numbers = get_fibonacci_numbers(number_of_items)
+    assert len(fib_numbers) == number_of_items
+    assert fib_numbers == expected
+
 
 class TestFibonacciGenerator(unittest.TestCase):
     """
     Tests the Fibonacci generator
     """
+    def setUp(self):
+        # Clear the cache each time before running any tests
+        get_fibonacci_numbers.cache_clear()
+
+
     def test_get_fibonacci_sequence(self):
         fib_numbers = get_fibonacci_numbers(10)
         assert isinstance(fib_numbers, list)
         assert fib_numbers == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-
-
-    def test_get_fibonacci_corner_cases(self):
-        fib_numbers = get_fibonacci_numbers(0)
-        assert isinstance(fib_numbers, list)
-        assert fib_numbers == []
 
 
     def test_get_fibonacci_sequence_fails_for_non_integer_argument(self):
@@ -25,7 +41,6 @@ class TestFibonacciGenerator(unittest.TestCase):
         assert str(ex.value) == expected_msg
 
 
-    # TODO Setup some parameterized tests
     def test_get_fibonacci_sequence_handles_negative_integer_argument(self):
         expected_msg = 'The quantity parameter must be >= 0. Invalid parameter=-1'
         with pytest.raises(ValueError) as ex:
